84642ee874b6644e3e9d43088c214ab37ebe43a4
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2019-06-20 14:54:30 +0100

    Polished ExtractionZone

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -2,32 +2,40 @@
 
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
-	/** Create a Box component called OverlapComponent
-	*	set collision of the Overlapcomponent
-	*	Set Response of collision of all channels
-	*	Set Response of collision of pawn, only.
-	*	Set the BoxExtent of the overlapComponent (x and y, in units)
-	*	Make Overlap the RootComponent
-	*/
-	OverlapComponent = CreateDefaultSubobject < UBoxComponent(TEXT("OverlapComponent"));
+	/**
+	 * Create a Box component called OverlapComponent
+	 * Set the collision of the overlapComponent
+	 * Set the extent of the Box (x, y and z, in units)
+	 * Make OverlapComponent the Root component
+	 */
+	OverlapComponent = CreateAbstractDefaultSubobject < UBoxComponent>(TEXT("OverlapComponent"));
 	OverlapComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	OverlapComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
 	OverlapComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-	OverlapComponent->SetBoxExtent(200.f);
+	OverlapComponent->SetBoxExtent(FVector(200.f));
 	RootComponent = OverlapComponent;
 
-	/** Bind to Event */
-	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap)
+	OverlapComponent->SetHiddenInGame(false);
+
+	/** Notify OverlapComponent when its overlapped */
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+	DecalComponent = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+	DecalComponent->DecalSize = FVector(200.f, 200.f, 200.f);
+	DecalComponent->SetupAttachment(RootComponent);
 }
 
-void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
 	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
 }
+
+
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "FPSExtractionZone.generated.h"
 
-class UBoxComponent
+class UBoxComponent;
 
 UCLASS()
 class FPSGAME_API AFPSExtractionZone : public AActor
@@ -20,10 +20,14 @@ public:
 protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-		UBoxComponent* OverlapComponent;
+	UBoxComponent* OverlapComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UDecalComponent* DecalComponent;
+
 
 	UFUNCTION()
-	void HandleOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-
+	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+	
 };
